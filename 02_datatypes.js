48dc688fc2a0af61b7aa4b6f6be7444fbafac321
Vaishnavi Biradar <vaishnavibiradar895@gmail.com>
2024-03-16 01:10:24 +0530

    02_typeOf
    02_typeOf

---
+++ 02_datatypes.js
@@ -0,0 +1,29 @@
+var age = 30;
+console.log("Age",age)
+
+var typeofAge = typeof age;
+console.log("Type of age is:", typeofAge);
+
+age = "Pune";
+var typeofAge = typeof age;
+console.log("After update age type id is:", typeofAge);
+
+age = true;
+var typeofAge = typeof age;
+console.log("The type of age is:", typeofAge);
+
+var city; // Declaration
+console.log("city:",city);
+var typeofCity = typeof city;
+console.log("Type of City:",typeofCity); //This gives u undefined beacuse we have declared but values is not given means initialization has not done//
+
+var pin = null;
+console.log();("pin: ",pin); 
+var typeofPin = typeof pin;
+console.log("Type of pin: ",typeofPin);
+
+var isMarried = true
+console.log("isMarried:", isMarried);
+var typeofMarried = typeof isMarried;
+console.log("Type of Married:", typeofMarried);
+
---
+++ 02_typeOf.js
@@ -0,0 +1,48 @@
+console.log("=========Step 1==========");
+
+var bankName;
+console.log("Bank name Before Initialization:",bankName);
+console.log("Datatype of Bank name:", bankName);
+
+bankName = "SBI Bank";
+var typeofBank = typeof bankName
+console.log("Bank name After Initialization:", bankName);
+console.log("Datatype of Bank name:", typeofBank);
+
+
+console.log("==========Step 2==========");
+var marks = '90%';
+console.log("Value of Marks:",marks);
+var typeofMarks = typeof marks;
+console.log("Datatype of Marks:",typeofMarks);
+
+
+console.log("==========Step 3==========");
+var isWorking;
+console.log("=====Before Initialization=====");
+console.log("isWorking value:", isWorking);
+console.log("Datatype of isWorking:", isWorking);
+
+var isWorking = true;
+var typeofWorking = typeof isWorking;
+console.log("=====After Initialization=====");
+console.log("isWorking value:",isWorking);
+console.log("Datatype of isWorking:", typeofWorking);
+
+
+console.log("==========Step 4==========");
+
+console.log("==1==");
+var totalCount = "Hundred and seven";
+console.log("Value of count:",totalCount);
+var typeofCount = typeof totalCount;
+console.log("Datatype of a count:",typeofCount);
+
+console.log("==2==");
+var totalCount = 107;
+var typeofCount = typeof totalCount;
+console.log("Value of count:", totalCount);
+console.log("Datatype of a count:",typeofCount);
+
+
+
