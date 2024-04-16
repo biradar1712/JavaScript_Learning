94ba8f940ce9d15f76a941f1de77c3f4a8f90a4b
Vaishnavi Biradar <vaishnavibiradar895@gmail.com>
2024-04-16 15:25:40 +0530

    06_factorial_assi_a.js
    06_factorial_assi_a.js

--- 06_factorial_Assi_A.js
+++ 06_factorial_Assi_A.js
@@ -3,6 +3,31 @@
 //factorial of that number call this function with below arguments as shown below, 
 //pls log the result in emaningful message
 
-function factorialOfNum(argu1) {
-    
-}
+function factorialOfNum(num) {
+    if (num === null || num === undefined || typeof num !== 'number' || num < 0) {
+        console.log("Invalid input : null");
+        return;
+    }
+    if (null === 0 || num === 1) {
+        console.log("The factorial of " + num +" is:  1");
+        return;
+    }else{
+        let factorial = 1;
+     for (let i = 2; i <= num ; i++) {
+        
+       factorial *= i; 
+     } 
+     console.log("The factorial of " + num +  " is:" + factorial);
+     return factorial;
+    }
+    }
+    factorialOfNum(5);
+    factorialOfNum(3);
+    factorialOfNum(null);
+    factorialOfNum(8);
+    factorialOfNum(undefined);
+    factorialOfNum(9);
+    factorialOfNum(0);
+
+
+
